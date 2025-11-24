c9a702f34b19caf88153b98f64af136a62570495
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-11-24 22:44:19 +0600

    module 11 video 6 complete

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,80 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
+import config from './config';
+import { RouteHandler, routes } from './helpers/RouteHandler';
+import './routes';
 
 const server: Server = http.createServer(
 	(req: IncomingMessage, res: ServerResponse)=>{
 		console.log('Server is running...');
 
-		if(req.url == '/' && req.method == 'GET'){
-			res.writeHead(200, { 'content-type': 'application/json'});
+		const method = req.method?.toUpperCase() || '';
+		const path = req.url || '';
+
+		const methodMap = routes.get(method);
+		const handler: RouteHandler | undefined = methodMap?.get(path);
+
+		if(handler){
+			handler(req, res);
+		}else{
+			res.writeHead(404, {'content-type': 'application/json'});
 			res.end(JSON.stringify({
-				message: 'Hello from node js with typescript...',
-				path: req.url,
-			}))
+				success: false,
+				message: 'Route not found!!!',
+				path: path,
+			}));
 		}
+
+		//root route
+		// if(req.url == '/' && req.method == 'GET'){
+		// 	res.writeHead(200, { 'content-type': 'application/json'});
+		// 	res.end(JSON.stringify({
+		// 		message: 'Hello from node js with typescript...',
+		// 		path: req.url,
+		// 	}))
+		// };
+
+		//health route
+		// if(req.url == '/api' && req.method == 'GET'){
+		// 	res.writeHead(200, { 'content-type': 'application/json'});
+		// 	res.end(
+		// 		JSON.stringify({
+		// 			message: 'Health status ok',
+		// 			path: req.url,
+		// 		})
+		// 	)
+		// };
+
+		//post method
+		// if(req.url == '/api/users' && req.method == 'POST'){
+		// 	// const user = {
+		// 	// 	id: 1,
+		// 	// 	name: 'alice',
+		// 	// };
+
+		// 	// res.writeHead(200, { 'content-type': 'application/json'});
+		// 	// res.end(JSON.stringify(user))
+
+		// 	let body = '';
+		// 	// listen for data chunk
+		// 	req.on('data', (chunk)=>{
+		// 		body += chunk.toString();
+		// 	});
+
+		// 	req.on('end', ()=>{
+		// 		try{
+		// 			const parseBody = JSON.parse(body);
+		// 			console.log(body);
+		// 			res.end(JSON.stringify(parseBody));
+		// 		}catch(err: any){
+		// 			console.log(err?.message);
+		// 		}
+		// 	});
+
+		// 	// res.end(JSON.stringify({message: 'processing...'}))
+		// };
 	}
 );
 
-server.listen(5000, ()=>{
-	console.log(`server is running on port ${5000}`);
+server.listen(config.port, ()=>{
+	console.log(`server is running on port ${config.port}`);
 });
